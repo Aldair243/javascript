6cbe87a85e658cd5b3df49e122a9a608d4fc0832
Aldair243 <jefersontimana9@gmail.com>
2024-10-22 12:15:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const taskInput = document.getElementById('taskInput');  
+const taskList = document.getElementById('taskList');  
 
+const newTask = document.createElement('li');  
+newTask.textContent = taskInput.value;  
+taskList.appendChild(newTask);  
+
+taskInput.value = '';  
+}
